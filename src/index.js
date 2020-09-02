dbb92db318f8614ba848e676e71855e00824fae9
SWFullPotential <swfullpotential@gmail.com>
2020-09-01 21:25:23 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,46 @@
+
 let addToy = false;
+let toyCollection = document.querySelector('#toy-collection')
+
+// fetch function defined
+function fetchToyObj() {
+  fetch("http://localhost:3000/toys")
+    .then(function(resp) {
+      return resp.json();
+    })
+    .then(function(json){
+      json.forEach(toy => renderToyObj(toy))
+    })
+;}
+//add toy info to card. 
+function renderToyObj(toy) {
+  let div = document.createElement('div')
+    div.className = "card"
+  let h2 = document.createElement('h2')
+    h2.innerText = toy["name"]
+  let img = document.createElement('img')
+    img.className = "toy-avatar"
+    img.src = toy["image"]
+  let likes = document.createElement('p')
+    likes.innerText = toy["likes"]
+  let likeBtn = document.createElement('button')
+    likeBtn.className = "like-btn"
+    likeBtn.innerText = "Like <3"
+    likeBtn.addEventListener('click', (e) => {
+      likes(toy, e)
+    })
+    div.appendChild(h2)
+    div.appendChild(img)
+    div.appendChild(likes)
+    div.appendChild(likeBtn)
+    toyCollection.appendChild(div)
+}
+
+// Fetch Andy's Toys call
+document.addEventListener('DOMContentLoaded', function() {
+  fetchToyObj()
+})
+// code below added by learn.co 
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
@@ -13,3 +55,12 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+/*
+<div class="card">
+<h2>Woody</h2>
+<img src=toy_image_url class="toy-avatar" />
+<p>4 Likes </p>
+<button class="like-btn">Like <3</button>
+</div>
+*/
